2f10849b232a8e6e6c3dabab7a5c82cd6ae252b3
Marco Bertini <m.bertini92@gmail.com>
2025-03-11 12:41:03 +0100

    changed main

--- main.f90
+++ main.f90
@@ -4,18 +4,23 @@ program main
 
   implicit none
 
-  integer, parameter :: d = 4
+  integer, parameter :: d = 8
   integer, dimension(d,d) :: m
   integer, dimension(d) :: ind
   integer :: sum
+  character(20) :: sum_str
+  integer :: i
+  integer :: j
 
-  m(1,1) = 78; m(1,2) = 74; m(1,3) = 54; m(1,4) = 55
-  m(2,1) = 77; m(2,2) = 18; m(2,3) = 63; m(2,4) = 15
-  m(3,1) = 32; m(3,2) = 61; m(3,3) = 12; m(3,4) = 74
-  m(4,1) = 34; m(4,2) = 34; m(4,3) = 43; m(4,4) =  4
+  do i = 1, d
+    read(*,*) m(i,:)
+  end do
 
-  call khun(d,m,ind,sum,.true.)
-  write(*,*) "ind = ", ind
-  write(*,*) "sum = ", sum
+!  call khun(d,m,ind,sum,verbose=.true.)
+  call khun(d,m,ind,sum,verbose=.false.)
+  write(*,'(8(I1))',advance="no") ind
+  write(sum_str,*) sum
+  sum_str = adjustl(sum_str)
+  write(*,'(A)') ":"//trim(sum_str)
 
 end program main
